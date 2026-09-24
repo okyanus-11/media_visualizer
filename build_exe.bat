6249717070d2203794ea9012418bfb75aeee22bd
okyanus-11 <okyanusbora11@gmail.com>
2026-09-24 20:15:12 +0300

    Add media visualizer application

---
+++ build_exe.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Bu komut dist klasorunde MediaVisualizer.exe dosyasini olusturur.
+pyinstaller --noconfirm --onefile --windowed --name MediaVisualizer main.py
+echo.
+echo Hazir: dist\MediaVisualizer.exe
+pause
+
